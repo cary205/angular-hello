50e6385fa4095d6798ef52b8464631fcf372d34c
cary205 <cary205@hotmail.com>
2019-05-27 14:04:17 +0800

    add protractor-ci.conf.js

---
+++ e2e/protractor-ci.conf.js
@@ -0,0 +1,10 @@
+const config = require('./protractor.conf.js').config;
+
+config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: ['--headless', '--no-sandbox']
+  }
+};
+
+exports.config = config;
